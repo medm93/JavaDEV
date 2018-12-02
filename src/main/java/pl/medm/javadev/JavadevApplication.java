c76bd20aaebe9be3b51dc306c4a21b959722a894
MEDM <m.e.misiak@gmail.com>
2018-12-02 18:24:27 +0100

    Dodanie projektu utworzonego przez Spring Initializr

---
+++ src/main/java/pl/medm/javadev/JavadevApplication.java
@@ -0,0 +1,12 @@
+package pl.medm.javadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavadevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavadevApplication.class, args);
+    }
+}
