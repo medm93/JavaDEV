339b824b9410e5834cfdfd13e41aa0e2d90aa17a
MEDM <m.e.misiak@gmail.com>
2018-12-27 20:35:05 +0100

    Poprawa pliku data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,5 +7,7 @@ VALUES ('ROLE_ADMIN');
 INSERT INTO role (role)
 VALUES ('ROLE_USER');
 
-INSERT INTO user_role (user_id, role_id)
-VALUES (1, 1);
+
+INSERT INTO user_role (user_role.user_id, user_role.role_id)
+VALUE ((SELECT u.id FROM user u WHERE u.email = 'admin@javadev.pl'),
+       (SELECT r.id FROM role r WHERE r.role = 'ROLE_ADMIN'));
