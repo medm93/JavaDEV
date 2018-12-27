cef3863a4c7be2e96c5d4f1ac40f5fcc6dd6440b
MEDM <m.e.misiak@gmail.com>
2018-12-27 20:24:16 +0100

    Poprawki w konfiguracji

--- src/main/java/pl/medm/javadev/configuration/security/CustomUserDetailsService.java
+++ src/main/java/pl/medm/javadev/configuration/security/CustomUserDetailsService.java
@@ -1,18 +1,30 @@
 package pl.medm.javadev.configuration.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import pl.medm.javadev.model.Role;
 import pl.medm.javadev.model.User;
 import pl.medm.javadev.repository.UserRepository;
 
-import java.util.HashSet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class CustomUserDetailsService implements UserDetailsService {
@@ -29,33 +41,68 @@ public class CustomUserDetailsService implements UserDetailsService {
         org.springframework.security.core.userdetails.User userDetails;
         User user;
 
-        user = userRepository.findByEmail(username);
+        user = userRepository.findByEmailOrIndexNumber(username, username);
         if (user != null) {
-            userDetails = new org.springframework.security.core.userdetails.User(
-                    user.getEmail(),
-                    user.getPassword(),
-                    convertAuthorities(user.getRoles())
-            );
-            return userDetails;
+            if (user.getEmail() != null) {
+                return new org.springframework.security.core.userdetails.User(
+                        user.getEmail(),
+                        user.getPassword(),
+                        convertAuthorities(user.getRoles())
+                );
+            }
+            if (user.getIndexNumber() != null) {
+                return new org.springframework.security.core.userdetails.User(
+                        user.getIndexNumber(),
+                        user.getPassword(),
+                        convertAuthorities(user.getRoles())
+                );
+            }
         }
-        user = userRepository.findByIndexNumber(username);
-        if (user != null) {
-            userDetails = new org.springframework.security.core.userdetails.User(
-                    user.getIndexNumber(),
-                    user.getPassword(),
-                    convertAuthorities(user.getRoles())
-            );
-            return userDetails;
-        }
-
         throw new UsernameNotFoundException("User not found");
     }
 
-    private Set<GrantedAuthority> convertAuthorities(Set<Role> userRoles) {
-        Set<GrantedAuthority> authorities = new HashSet<>();
-        for(Role ur: userRoles) {
-            authorities.add(new SimpleGrantedAuthority(ur.getRole()));
+    private Set<GrantedAuthority> convertAuthorities(Set<Role> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRole()))
+                .collect(Collectors.toSet());
+    }
+
+    @Component
+    public static class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+        @Override
+        public void commence(HttpServletRequest request, HttpServletResponse response,
+                             AuthenticationException exception) throws IOException {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
         }
-        return authorities;
+    }
+
+    @Component
+    public static class MySavedRequestAwareAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+        private RequestCache requestCache = new HttpSessionRequestCache();
+
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                            Authentication authentication) throws ServletException, IOException {
+
+            SavedRequest savedRequest = requestCache.getRequest(request, response);
+
+            if (savedRequest == null) {
+                clearAuthenticationAttributes(request);
+                return;
+            }
+            String targetUrlParam = getTargetUrlParameter();
+            if (isAlwaysUseDefaultTargetUrl() || (targetUrlParam != null && StringUtils.hasText(request.getParameter(targetUrlParam)))) {
+                requestCache.removeRequest(request, response);
+                clearAuthenticationAttributes(request);
+                return;
+            }
+
+            clearAuthenticationAttributes(request);
+        }
+
+        public void setRequestCache(RequestCache requestCache) {
+            this.requestCache = requestCache;
+        }
     }
 }
--- src/main/java/pl/medm/javadev/configuration/security/SecurityConfig.java
+++ src/main/java/pl/medm/javadev/configuration/security/SecurityConfig.java
@@ -1,10 +1,10 @@
-package pl.medm.javadev.configuration;
+package pl.medm.javadev.configuration.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,17 +13,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+    private CustomUserDetailsService.RestAuthenticationEntryPoint restAuthenticationEntryPoint;
     @Autowired
-    private MySavedRequestAwareAuthenticationSuccessHandler mySavedRequestAwareAuthenticationSuccessHandler;
+    private CustomUserDetailsService.MySavedRequestAwareAuthenticationSuccessHandler mySavedRequestAwareAuthenticationSuccessHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        return passwordEncoder;
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Override
@@ -36,28 +36,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/logout").permitAll()
-                //WYŚWIETLANIE LISTY UŻYTKOWNIKÓW
-                .antMatchers(HttpMethod.GET, "/user").hasAnyRole( "ADMIN")
-                //DODAWANIE UŻYTKOWNIKÓW
-                .antMatchers(HttpMethod.POST, "/user").hasAnyRole( "ADMIN")
-                //EDYTOWANIE DANYCH UŻYTKOWNIKA
-                .antMatchers(HttpMethod.PUT, "/user/{id}").hasAnyRole( "USER", "ADMIN")
-                //USUWANIE UŻYTKOWNIKA
-                .antMatchers(HttpMethod.DELETE, "/user/{id}").hasAnyRole( "ADMIN")
-                //ZMIANA HASŁA UŻYTKOWNIKA
-                .antMatchers(HttpMethod.PUT, "/user/{id}/password").hasAnyRole("USER", "ADMIN")
-                //WYŚWIETLANIE LISTY WYKŁADÓW
-                .antMatchers(HttpMethod.GET, "/lectures").hasAnyRole("USER", "ADMIN")
-                //DODAWANIE WYKŁADU
-                .antMatchers(HttpMethod.POST,"/lectures").hasAnyRole("ADMIN")
-                //WYŚWIETLANIE SZCZEGÓŁÓW WYKŁADU
-                .antMatchers(HttpMethod.GET,"/lectures/{id}").hasAnyRole("USER", "ADMIN")
-                //EDYCJA WYKŁADU
-                .antMatchers(HttpMethod.PUT,"/lectures/{id}").hasAnyRole("ADMIN")
-                //USUWANIE WYKŁADU
-                .antMatchers(HttpMethod.DELETE,"/lectures/{id}").hasAnyRole("ADMIN")
-                //WPISYWANIE SIĘ NA LISTĘ OBECNOŚCI
-                .antMatchers(HttpMethod.POST,"/lectures/{id}/users").hasAnyRole("USER")
+//                //WYŚWIETLANIE LISTY UŻYTKOWNIKÓW
+////                .antMatchers(HttpMethod.GET, "/user").hasAnyRole( "ADMIN")
+//                //DODAWANIE UŻYTKOWNIKÓW
+//                .antMatchers(HttpMethod.POST, "/user").hasAnyRole( "ADMIN")
+//                //EDYTOWANIE DANYCH UŻYTKOWNIKA
+//                .antMatchers(HttpMethod.PUT, "/user/{id}").hasAnyRole( "USER", "ADMIN")
+//                //USUWANIE UŻYTKOWNIKA
+//                .antMatchers(HttpMethod.DELETE, "/user/{id}").hasAnyRole( "ADMIN")
+//                //ZMIANA HASŁA UŻYTKOWNIKA
+//                .antMatchers(HttpMethod.PUT, "/user/{id}/password").hasAnyRole("USER", "ADMIN")
+//                //WYŚWIETLANIE LISTY WYKŁADÓW
+//                .antMatchers(HttpMethod.GET, "/lectures").hasAnyRole("USER", "ADMIN")
+//                //DODAWANIE WYKŁADU
+//                .antMatchers(HttpMethod.POST,"/lectures").hasAnyRole("ADMIN")
+//                //WYŚWIETLANIE SZCZEGÓŁÓW WYKŁADU
+//                .antMatchers(HttpMethod.GET,"/lectures/{id}").hasAnyRole("USER", "ADMIN")
+//                //EDYCJA WYKŁADU
+//                .antMatchers(HttpMethod.PUT,"/lectures/{id}").hasAnyRole("ADMIN")
+//                //USUWANIE WYKŁADU
+//                .antMatchers(HttpMethod.DELETE,"/lectures/{id}").hasAnyRole("ADMIN")
+//                //WPISYWANIE SIĘ NA LISTĘ OBECNOŚCI
+//                .antMatchers(HttpMethod.POST,"/lectures/{id}/users").hasAnyRole("USER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
