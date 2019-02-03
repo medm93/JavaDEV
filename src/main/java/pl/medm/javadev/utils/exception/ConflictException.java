34bcfa3dc05598061994eb2f3871d06170cbecaa
MEDM <m.e.misiak@gmail.com>
2019-02-03 18:39:02 +0100

    Zmiany w wyjątkach

--- src/main/java/pl/medm/javadev/utils/exception/ConflictException.java
+++ src/main/java/pl/medm/javadev/utils/exception/ConflictException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
-public class RoleExistsException extends RuntimeException {
+public class ConflictException extends RuntimeException {
 
-    public RoleExistsException(String message) {
+    public ConflictException(String message) {
         super(message);
     }
 }
--- src/main/java/pl/medm/javadev/utils/exception/ForbiddenException.java
+++ src/main/java/pl/medm/javadev/utils/exception/ForbiddenException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.FORBIDDEN)
-public class RoleForbiddenException extends RuntimeException {
-    public RoleForbiddenException(String message) {
+public class ForbiddenException extends RuntimeException {
+    public ForbiddenException(String message) {
         super(message);
     }
 }
