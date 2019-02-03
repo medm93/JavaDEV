775da281a6619b6cf21504dee022a114c40584f0
MEDM <m.e.misiak@gmail.com>
2019-02-03 18:33:18 +0100

    Zmiana nazw wyjątków oraz usunięcie zbędnych klas

--- src/test/resources/data-lecture-test.sql
+++ src/test/resources/data-lecture-test.sql
@@ -1,8 +1,8 @@
-INSERT INTO user (id, first_Name, last_Name, email, password)
-VALUES (1, 'James', 'Bond', 'agent007@uk.com', '{noop}zaq1@WSX');
+INSERT INTO user (first_Name, last_Name, email, password)
+VALUES ('James', 'Bond', 'agent007@uk.com', '{noop}zaq1@WSX');
 
-INSERT INTO user (id, first_Name, last_Name, email, password)
-VALUES (2, 'Steven', 'Rogers', 'capitan.ameryka@marvel.com', '{noop}zaq1@WSX');
+INSERT INTO user (first_Name, last_Name, email, password)
+VALUES ('Steven', 'Rogers', 'capitan.ameryka@marvel.com', '{noop}zaq1@WSX');
 
 INSERT INTO lecture(id, title, description, lecturer, completed)
 VALUES (1, 'Java 8', 'The basics of language', 'Tony Stark', true);
