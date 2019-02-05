85a72735411b89c1bab09704d8c0ca37faf3a703
MEDM <m.e.misiak@gmail.com>
2019-02-05 11:01:49 +0100

    Poprawa klas modeli

--- src/main/java/pl/medm/javadev/model/entity/Role.java
+++ src/main/java/pl/medm/javadev/model/entity/Role.java
@@ -9,12 +9,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Role {
+public class Role implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/pl/medm/javadev/model/entity/User.java
+++ src/main/java/pl/medm/javadev/model/entity/User.java
@@ -2,12 +2,15 @@ package pl.medm.javadev.model.entity;
 
 import lombok.*;
 import pl.medm.javadev.constraint.Password;
-import pl.medm.javadev.constraint.group.UserData;
-import pl.medm.javadev.constraint.group.UserPassword;
+import pl.medm.javadev.constraint.group.BasicData;
+import pl.medm.javadev.constraint.group.PasswordData;
+import pl.medm.javadev.constraint.group.StudiesData;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -22,17 +25,28 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotEmpty(message = "pl.medm.javadev.model.User.firstName.NotEmpty", groups = {UserData.class})
+    @NotEmpty(groups = BasicData.class)
+    @Column(name = "first_name")
     private String firstName;
-    @NotEmpty(message = "pl.medm.javadev.model.User.lastName.NotEmpty", groups = {UserData.class})
+    @NotEmpty(groups = BasicData.class)
+    @Column(name = "last_name")
     private String lastName;
-    @Email(message = "pl.medm.javadev.model.User.email.Email", groups = {UserData.class})
-    @NotEmpty(message = "pl.medm.javadev.model.User.email.NotEmpty", groups = {UserData.class})
+    @Email(groups = BasicData.class)
+    @NotEmpty(groups = BasicData.class)
     private String email;
-    @Password(message = "pl.medm.javadev.model.User.password.Password", groups = UserPassword.class)
+    @Password(groups = PasswordData.class)
     private String password;
+    @Size(min = 1, max = 1, groups = StudiesData.class)
+    @Pattern(regexp = "^[0-5]+$", groups = StudiesData.class)
+    @Column(name = "year_of_study")
     private String yearOfStudy;
+    @Size(min = 5, groups = StudiesData.class)
+    @Pattern(regexp = "^[a-zA-Z]+$", groups = StudiesData.class)
+    @Column(name = "field_of_study")
     private String fieldOfStudy;
+    @Size(min = 6, max = 6, groups = StudiesData.class)
+    @Pattern(regexp = "^[0-9]+$", groups = StudiesData.class)
+    @Column(name = "index_number")
     private String indexNumber;
     @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.ALL})
     @JoinTable(
@@ -56,5 +70,35 @@ public class User implements Serializable {
         this.lastName = lastName;
         this.email = email;
         this.password = password;
+    }
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String firstName, String lastName, String email, String password, String yearOfStudy,
+                String fieldOfStudy, String indexNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.yearOfStudy = yearOfStudy;
+        this.fieldOfStudy = fieldOfStudy;
+        this.indexNumber = indexNumber;
+    }
+
+    public User(long id, String firstName, String lastName, String email, String password, String yearOfStudy,
+                String fieldOfStudy, String indexNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.yearOfStudy = yearOfStudy;
+        this.fieldOfStudy = fieldOfStudy;
+        this.indexNumber = indexNumber;
     }
 }
